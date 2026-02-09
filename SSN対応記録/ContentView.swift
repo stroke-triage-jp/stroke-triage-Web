0317db4e80fb68db9a313b3725ac921334780461
好績佐藤 <y4nrs10@gmail.com>
2026-02-09 22:43:48 +0900

    Initial Commit

---
+++ SSN対応記録/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSN対応記録
+//
+//  Created by 佐藤好績 on 2026/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
